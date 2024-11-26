89c11589f182ed32c8112e0d0f6de05913ed8f65
Jacky Cheng <chjack568@gmail.com>
2024-11-26 15:48:53 -0800

    Add Clerk sign-in/sign-up functionality.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
 'use client'
 
+import { useUser, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <AppBar position="static">
+      <Container maxWidth="lg">
+        <Toolbar>
+          <Typography variant="h6" style={{ flexGrow: 1 }}>Zotmate - Digital Assistant for UCI</Typography>
+          <SignedOut>
+            <Button color="inherit" href="/sign-in">Login</Button>
+            <Button color="inherit" href="/sign-up">Sign Up</Button>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,33 @@
+import { SignIn } from '@clerk/nextjs';
+import Link from 'next/link';
+import { Container, AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+
+export default function SignInPage() {
+    return (
+        <Box>
+            <AppBar position="static">
+                <Container maxWidth="lg">
+                    <Toolbar>
+                        <Typography variant="h6" sx={{ flexGrow: 1 }}>Zotmate - Digital Assistant for UCI</Typography>
+                        <Button color="inherit" component={Link} href="/sign-in">
+                            Login
+                        </Button>
+                        <Button color="inherit" component={Link} href="/sign-up">
+                            Sign Up
+                        </Button>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+            <Box sx={{ mt: { xs: 2, sm: 3, md: 4 } }}>
+                <Box 
+                    display="flex" 
+                    flexDirection="column" 
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    <SignIn />
+                </Box>
+            </Box>
+        </Box>
+    );
+}
