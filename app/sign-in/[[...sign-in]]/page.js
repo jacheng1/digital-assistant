c1351b2613fc2f2fd27c1164e27cfa787e2ea3e9
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2024-11-27 23:03:06 -0800

    Add basic layout for pages.

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,6 +1,9 @@
 import { SignIn } from '@clerk/nextjs';
 import Link from 'next/link';
 import { Container, AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import HomeIcon from '@mui/icons-material/Home';
+import ListIcon from '@mui/icons-material/List';
+import EditNoteIcon from '@mui/icons-material/EditNote';
 
 export default function SignInPage() {
     return (
@@ -8,12 +11,26 @@ export default function SignInPage() {
             <AppBar position="static">
                 <Container maxWidth="lg">
                     <Toolbar>
-                        <Typography variant="h6" sx={{ flexGrow: 1 }}>Zotmate - Digital Assistant for UCI</Typography>
-                        <Button color="inherit" component={Link} href="/sign-in">
-                            Login
-                        </Button>
+                        <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
+                            <Typography variant="h6">Zotmate - Digital Assistant for UCI</Typography>
+                            <Button color="inherit" component={Link} href="/" sx={{ ml: 2 }}>
+                                Home
+                                <HomeIcon sx={{ ml: 0.5 }} />
+                            </Button>
+                            <Button color="inherit" component={Link} href="/todo-list" sx={{ ml: 2 }}>
+                                To-Do List
+                                <ListIcon sx={{ ml: 0.5 }} />
+                            </Button>
+                            <Button color="inherit" component={Link} href="/notes" sx={{ ml: 2 }}>
+                                Notes
+                                <EditNoteIcon sx={{ ml: 0.5 }} />
+                            </Button>
+                        </Box>
                         <Button color="inherit" component={Link} href="/sign-up">
-                            Sign Up
+                            Get Started
+                        </Button>
+                        <Button color="inherit" component={Link} href="/sign-in">
+                            Sign In
                         </Button>
                     </Toolbar>
                 </Container>
