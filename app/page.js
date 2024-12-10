8c954029274f2004fc6bcac3993cd0320ee315fb
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2024-12-09 23:08:49 -0800

    Complete homepage, time/weather pending.

--- app/page.js
+++ app/page.js
@@ -7,57 +7,71 @@ import ListIcon from '@mui/icons-material/List';
 import EditNoteIcon from '@mui/icons-material/EditNote';
 import { useUser, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
-import bot_icon from './assets/bot.png';
+import bot from './assets/bot.png';
+import weather_background from './assets/weather_background.png';
 
 export default function Home() {
   return (
     <Box sx={{ backgroundColor: "#FFFFFF", minHeight: '100vh' }}>
-      <AppBar position="static" sx={{ backgroundColor: "white", boxShadow: "none" }}>
+      <AppBar position="static" sx={{ backgroundColor: "white", boxShadow: "none", borderBottom: "1px solid #EBEBEB" }}>
         <Container maxWidth="lg">
           <Toolbar>
             <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
-              <Typography variant="h6" sx={{ color: "#2196f3" }}>Zotmate - Digital Assistant for UCI</Typography>
-              <Button color="inherit" component={Link} href="/" sx={{ color: "#2196f3", ml: 3 }}>
-                Home
-                <HomeIcon sx={{ ml: 0.5 }} />
-              </Button>
-              <Button color="inherit" component={Link} href="/todo-list" sx={{ color: "#2196f3", ml: 3 }}>
-                To-Do List
-                <ListIcon sx={{ ml: 0.5 }} />
-              </Button>
-              <Button color="inherit" component={Link} href="/notes" sx={{ color: "#2196f3", ml: 3 }}>
-                Notes
-                <EditNoteIcon sx={{ ml: 0.5 }} />
-              </Button>
+            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+              <Box component="span" sx={{ color: "#000000" }}>Zot</Box>
+              <Box component="span" sx={{ color: "#5fc4d2" }}>Mate</Box>
+            </Typography>
+              <Box sx={{ marginLeft: 15, display: 'flex', gap: 3 }}>
+                <Button color="inherit" component={Link} href="/" sx={{ color: "#5fc4d2", fontWeight: "bold" }}>
+                  Home
+                  <HomeIcon sx={{ ml: 0.5 }} />
+                </Button>
+                <Button color="inherit" component={Link} href="/todo-list" sx={{ color: "#000000", fontWeight: "bold" }}>
+                  To-Do List
+                  <ListIcon sx={{ ml: 0.5 }} />
+                </Button>
+                <Button color="inherit" component={Link} href="/notes" sx={{ color: "#000000", fontWeight: "bold" }}>
+                  Notes
+                  <EditNoteIcon sx={{ ml: 0.5 }} />
+                </Button>
+              </Box>
             </Box>
             <SignedOut>
-              <Button 
-                color="inherit" 
+              <Button
+                variant="outlined"
                 component={Link} 
-                href="/sign-up" 
+                href="/sign-up"
                 sx={{ 
-                  color: "#FFFFFF", 
-                  backgroundColor: "#2196f3", 
+                  color: "#5fc4d2",
                   ml: 2,
-                  borderRadius: "10px",
+                  borderRadius: "8px",
+                  paddingTop: 1.5,
+                  paddingBottom: 1.5,
+                  paddingLeft: 3,
+                  paddingRight: 3,
+                  borderColor: "#5fc4d2",
                   "&:hover": {
-                    backgroundColor: "#68B5F0"
+                    color: "#FFFFFF",
+                    backgroundColor: "#5fc4d2"
                   }
                 }}
               >
                 Get Started
               </Button>
-              <Button 
-                color="inherit"
+              <Button
                 component={Link} 
                 href="/sign-in" 
                 sx={{ 
                   color: "#FFFFFF", 
-                  backgroundColor: "#2196f3", 
+                  backgroundColor: "#5fc4d2", 
                   ml: 2,
-                  borderRadius: "10px",
+                  borderRadius: "8px",
+                  paddingTop: 1.5,
+                  paddingBottom: 1.5,
+                  paddingLeft: 3,
+                  paddingRight: 3,
                   "&:hover": {
-                    backgroundColor: "#68B5F0"
+                    backgroundColor: "#bbf4fe"
                   }
                 }}
               >
@@ -73,14 +87,83 @@ export default function Home() {
       <Container maxWidth="lg">
         <Box
           sx={{
-            padding: 3,
             textAlign: "left",
             display: "flex",
             alignItems: "center",
+            justifyContent: "space-between"
           }}
         >
-          <Typography variant="h1" sx={{ color: "#2196f3", marginRight: 2 }}>Hello, Guest!</Typography>
-          <Image src={bot_icon} alt="Bot Icon" />
+          <Box sx={{ marginTop: "-130px" }}>
+            <Typography variant="h1" sx={{ fontWeight: "bold", marginBottom: 2 }}>
+              <Box component="span" sx={{ color: "#000000" }}>Welcome to Zot</Box>
+              <Box component="span" sx={{ color: "#5fc4d2" }}>Mate</Box>
+              <Box component="span" sx={{ color: "#000000" }}>!</Box>
+            </Typography>
+            <Typography variant="h6" sx={{ color: "#000000" }}>
+              Your personal digital assistant for a seamless experience at the University of California, Irvine!
+            </Typography>
+            <Box sx={{ marginTop: "30px" }}>
+              <Button
+                component={Link} 
+                href="/todo-list" 
+                sx={{ 
+                  color: "#FFFFFF", 
+                  backgroundColor: "#5fc4d2", 
+                  ml: 2,
+                  borderRadius: "8px",
+                  paddingTop: 2,
+                  paddingBottom: 2,
+                  paddingLeft: 4.5,
+                  paddingRight: 4.5,
+                  "&:hover": {
+                    backgroundColor: "#bbf4fe"
+                  }
+                }}
+              >
+                To-Do List
+              </Button>
+              <Button
+                component={Link} 
+                href="/notes" 
+                sx={{ 
+                  color: "#FFFFFF", 
+                  backgroundColor: "#5fc4d2", 
+                  ml: 2,
+                  borderRadius: "8px",
+                  marginLeft: "30px",
+                  paddingTop: 2,
+                  paddingBottom: 2,
+                  paddingLeft: 4.5,
+                  paddingRight: 4.5,
+                  "&:hover": {
+                    backgroundColor: "#bbf4fe"
+                  }
+                }}
+              >
+                Notes
+              </Button>
+            </Box>
+          </Box>
+          <Image src={bot} alt="Bot Icon" />
+        </Box>
+      </Container>
+      <Container maxWidth="lg">
+        <Box
+          sx={{
+            backgroundImage: `url(${weather_background.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            borderRadius: "8px",
+            boxShadow: 2,
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            height: "200px",
+          }}
+        >
+          <Typography variant="h6" sx={{ color: "#FFFFFF" }}>Placeholder</Typography>
         </Box>
       </Container>
     </Box>
