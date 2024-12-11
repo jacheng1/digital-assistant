e06cfc5c2ca600cb6458ce31725a05ef5693cef3
Jacky Cheng <75710146+jacheng1@users.noreply.github.com>
2024-12-10 19:59:47 -0800

    Modify sign-in, sign-up page UI.

--- app/sign-up/[[...sign-up]]/page.js
+++ app/sign-up/[[...sign-up]]/page.js
@@ -15,40 +15,93 @@ import EditNoteIcon from "@mui/icons-material/EditNote";
 export default function SignUpPage() {
   return (
     <Box>
-      <AppBar position="static">
+      <AppBar
+        position="static"
+        sx={{
+          backgroundColor: "white",
+          boxShadow: "none",
+          borderBottom: "1px solid #EBEBEB",
+        }}
+      >
         <Container maxWidth="lg">
           <Toolbar>
             <Box display="flex" alignItems="center" sx={{ flexGrow: 1 }}>
-              <Typography variant="h6">
-                Zotmate - Digital Assistant for UCI
+              <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                <Box component="span" sx={{ color: "#000000" }}>
+                  Zot
+                </Box>
+                <Box component="span" sx={{ color: "#5fc4d2" }}>
+                  Mate
+                </Box>
               </Typography>
-              <Button color="inherit" component={Link} href="/" sx={{ ml: 2 }}>
-                Home
-                <HomeIcon sx={{ ml: 0.5 }} />
-              </Button>
-              <Button
-                color="inherit"
-                component={Link}
-                href="/todo-list"
-                sx={{ ml: 2 }}
-              >
-                To-Do List
-                <ListIcon sx={{ ml: 0.5 }} />
-              </Button>
-              <Button
-                color="inherit"
-                component={Link}
-                href="/notes"
-                sx={{ ml: 2 }}
-              >
-                Notes
-                <EditNoteIcon sx={{ ml: 0.5 }} />
-              </Button>
+              <Box sx={{ marginLeft: 15, display: "flex", gap: 3 }}>
+                <Button
+                  color="inherit"
+                  component={Link}
+                  href="/"
+                  sx={{ color: "#5fc4d2", fontWeight: "bold" }}
+                >
+                  Home
+                  <HomeIcon sx={{ ml: 0.5 }} />
+                </Button>
+                <Button
+                  color="inherit"
+                  component={Link}
+                  href="/todo-list"
+                  sx={{ color: "#000000", fontWeight: "bold" }}
+                >
+                  To-Do List
+                  <ListIcon sx={{ ml: 0.5 }} />
+                </Button>
+                <Button
+                  color="inherit"
+                  component={Link}
+                  href="/notes"
+                  sx={{ color: "#000000", fontWeight: "bold" }}
+                >
+                  Notes
+                  <EditNoteIcon sx={{ ml: 0.5 }} />
+                </Button>
+              </Box>
             </Box>
-            <Button color="inherit" component={Link} href="/sign-up">
+            <Button
+              variant="outlined"
+              component={Link}
+              href="/sign-up"
+              sx={{
+                color: "#5fc4d2",
+                ml: 2,
+                borderRadius: "8px",
+                paddingTop: 1.5,
+                paddingBottom: 1.5,
+                paddingLeft: 3,
+                paddingRight: 3,
+                borderColor: "#5fc4d2",
+                "&:hover": {
+                  color: "#FFFFFF",
+                  backgroundColor: "#5fc4d2",
+                },
+              }}
+            >
               Get Started
             </Button>
-            <Button color="inherit" component={Link} href="/sign-in">
+            <Button
+              component={Link}
+              href="/sign-in"
+              sx={{
+                color: "#FFFFFF",
+                backgroundColor: "#5fc4d2",
+                ml: 2,
+                borderRadius: "8px",
+                paddingTop: 1.5,
+                paddingBottom: 1.5,
+                paddingLeft: 3,
+                paddingRight: 3,
+                "&:hover": {
+                  backgroundColor: "#bbf4fe",
+                },
+              }}
+            >
               Sign In
             </Button>
           </Toolbar>
